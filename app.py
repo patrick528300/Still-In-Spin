7df500b0bbfa720becb35ab0fad59767bfe2b341
Patrick Deng <patrickdeng@Patricks-MacBook-Air.local>
2025-06-16 18:57:26 -0700

    second change

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("vehicle.html")
 
 @app.route("/add",methods=["POST"])
 def add_record():
     return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
