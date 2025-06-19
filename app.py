4c2249adaf46dd9908850e4f9dfe2b1a50e38e05
Patrick Deng <patrickdeng@Patricks-MacBook-Air.local>
2025-06-19 16:55:53 -0700

    third

--- app.py
+++ app.py
@@ -2,32 +2,33 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 id = 1
-vehicles = {}
-class Vehicle:
-    def __init__(self,id,year,make,model,color,mileage):
-        self.id = id
-        self.year = year
-        self.make = make
-        self.model = model
-        self.color = color
-        self.init_mileage = mileage
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/addVehicle",methods=["POST"])
+@app.route("/addVehicle",methods=["GET"])
 def addVehicle():
-    year = request.form['year']
-    make = request.form['make']
-    model = request.form['model']
-    color = request.form['color']
-    init_mileage = request.form['mileage']
-    car = Vehicle(id,year,make,model,color,init_mileage)
-    id+=1
-    return redirect("home.html",
-                    id=car.id,year=car.year,model=car.model,
-                    color=car.color,init_mileage=car.init_mileage)
+    return render_template("addVehicle.html")
+
+@app.route("/vehicle",methods=["GET","POST"])
+def handleVehicle():
+    if request.method == "POST":
+        year = request.form['year']
+        make = request.form['make']
+        model = request.form['model']
+        color = request.form['color']
+        init_mileage = request.form['mileage']
+        id+=1
+        print("Year: ", year)
+        print("Make: ", make)
+        print("Model: ", model)
+        print("Color: ", color)
+        print("initial mileage: ", init_mileage)
+        return redirect("home.html",
+                    id=id,year=year,model=model,
+                    color=color,init_mileage=init_mileage)
+    return render_template("addVehicle.html")
 
 
 if __name__ == "__main__":
